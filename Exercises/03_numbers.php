01bc448d6cfe5c86412e8341befa92341c424c6f
Nikzli <nikini.kawya123@gmail.com>
2021-05-24 16:56:55 +0530

    php numbers

--- Exercises/03_numbers.php
+++ Exercises/03_numbers.php
@@ -1,7 +1,16 @@
 <?php
-$num="The absolute number of -1.5 is  ".abs(-1.5)."<br>";
-$num1="The absolute number of 32 is  ". abs(32)."<br>";
+$num="The absolute number is  ".abs(-1.5)."<br>";
+$num1="The absolute number is  ". abs(32)."<br>";
+$num3="The absolute mumber is  ". abs(-1432)."<br>";
 
 echo $num;
 echo $num1;
+echo $num3. "<br>";
+
+//method2
+
+echo "abs1 = " .abs(-1.5)."<br>";
+echo "abs2 = " .abs(32)."<br>";
+echo "abs3 = " .abs(-1432)."<br>";
+
 ?>
