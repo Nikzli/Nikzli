51de90e09a0cbc1f006c1c60f792ba7b44b9e0ad
Nikzli <84511062+Nikzli@users.noreply.github.com>
2021-05-30 13:59:08 +0530

    php array

--- Exercises/05_array.php
+++ Exercises/05_array.php
@@ -10,15 +10,5 @@ echo $color[2]."<br>";
 echo $color[4]."<br>";
 
 =======
-<?php
-$color= array("black", "green", "blue", "red", "white");
 
-//first
-echo $color[0]."<br>";
-//thrid
-echo $color[2]."<br>";
-//fourth
-echo $color[4]."<br>";
-
->>>>>>> f909e70dc6e7ca30be28a6a3c0e261316b13b450
 ?>
