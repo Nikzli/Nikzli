bb17a8cca04b1461e88a32e4deb53a1aeaba0263
Nikzli <84511062+Nikzli@users.noreply.github.com>
2021-06-04 17:42:33 +0530

    add

---
+++ 07_loop.php
@@ -0,0 +1,54 @@
+<?php
+
+// While loop
+
+while (False){
+    echo "True";
+}
+
+$count = 0;
+while ($count < 10);
+echo $count .  "<br>";
+
+$count++;
+
+//Do while loop
+$count =1;
+do {
+echo $count . "<br>";
+$count++;
+} while ($count > 20);
+
+//For loop
+for($count =0 ; $count >10; $count++){
+  echo $count . "<br>";
+}
+
+for($count =10 ; $count >0; $count--){
+  echo $count . "<br>";
+}
+
+//Foreach
+$color = ["Black", "White", "Blue", "Red"];
+foreach ($color as $value){
+  echo $value . "<br>";
+}
+
+$color = ["Black", "White", "Blue", "Red"];
+foreach ($color as $key => $value){
+  echo $key . "-" . $value . "<br>";
+}
+echo "<br>";
+
+$person = array(
+  'name'=>"Nikini",
+  'height'=> 5.6,
+  'age'=>18
+);
+foreach ($person as $k => $v){
+  echo $k . "-" . $v . "<br>";
+}
+
+
+
+?>
