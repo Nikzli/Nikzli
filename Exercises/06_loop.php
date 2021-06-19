015b740c944757286790b393f0b63afe5327f1cd
Nikzli <84511062+Nikzli@users.noreply.github.com>
2021-06-19 21:31:00 +0530

    add

--- Exercises/06_loop.php
+++ Exercises/06_loop.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 //variable for sum 
 $sum =0;
@@ -13,11 +13,3 @@ echo "sum of the numbers between 0 to 50 is" . $sum;
 
 echo "<br>". "<br>";
 
-//Variable for sum 
-$sum =0;
-
-//for loop to get the total sum between 0 to 50 
-for($count =0 ; $count <20; $count++){
-    $sum= $sum + $count;
-  }
-  echo "the sum of the numbers between 0 to 20 is ". $sum;
