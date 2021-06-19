c1ebbbf8b97ae40b104c09673165a271058a2ffa
Nikzli <84511062+Nikzli@users.noreply.github.com>
2021-06-19 21:22:26 +0530

    add

---
+++ 10_include/contact.php
@@ -0,0 +1,9 @@
+<?php
+include "layout/header.php";
+?>
+    <h1>Contact page</h1>
+<?php
+include "layout/footer.php";
+?>
+
+    
---
+++ 10_include/functions.php
@@ -0,0 +1,5 @@
+<?php
+function storename(){
+    return "Sunny Stores";
+}
+?>
